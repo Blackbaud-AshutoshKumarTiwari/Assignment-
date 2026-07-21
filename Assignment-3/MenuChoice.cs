f7c1cf653ae967d63faa5325e49141d9a38e9005
Blackbaud-AshutoshKumarTiwari <ashutosh.kumartiwari@blackbaud.me>
2026-07-21 16:29:42 +0530

    Final Solid ASS

--- Assignment-3/MenuChoice.cs
+++ Assignment-3/MenuChoice.cs
@@ -8,3 +8,4 @@
     Power = 6,
     ClearHistory = 7
 }
+// The MenuChoice enum represents the different operations available in the calculator application. Each operation is assigned a unique integer value, starting from 1 for Addition and incrementing by 1 for each subsequent operation. The ClearHistory option is also included as the last choice in the menu.
